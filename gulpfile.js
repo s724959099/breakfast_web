86c9fe795d7e8015c4586b37deed6d7d902b5e70
meatball <mb0520@sinyi-tech.com>
2017-11-14 17:48:45 +0800

    add css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 /**
  * Created by Admin on 2017/11/14.
  */
+var gulp = require('gulp'),               // 載入 gulp
+    gulpSass = require('gulp-sass');    // 載入 gulp-sass
+    concat = require('gulp-concat');
+
+gulp.task('watch', function () {
+    gulp.watch('static/sass/**/*.sass', ['styles']);
+});
+
+gulp.task('styles', function () {
+    gulp.src('static/sass/**/*.sass')    // 指定要處理的 Scss 檔案目錄
+        .pipe(gulpSass({          // 編譯 Scss
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('static/css'));  // 指定編譯後的 css 檔案目錄
+});
