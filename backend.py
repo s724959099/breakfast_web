b26e3b9ac0cb6a42f80b700b7a1c9cd6f0fbcf34
meatball <mb0520@sinyi-tech.com>
2017-11-14 22:34:09 +0800

    add gulp

--- backend.py
+++ backend.py
@@ -22,4 +22,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=2000, debug=True)
--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@
 var gulp = require('gulp'),               // 載入 gulp
     gulpSass = require('gulp-sass');    // 載入 gulp-sass
     concat = require('gulp-concat');
+    livereload = require('gulp-livereload');
 
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('static/sass/**/*.sass', ['styles']);
+    gulp.watch('templates/**/*.html', ['styles']);
 });
 
 gulp.task('styles', function () {
@@ -15,5 +18,6 @@ gulp.task('styles', function () {
             outputStyle: 'compressed'
         }))
         .pipe(concat('app.css'))
-        .pipe(gulp.dest('static/css'));  // 指定編譯後的 css 檔案目錄
+        .pipe(gulp.dest('static/css'))  // 指定編譯後的 css 檔案目錄
+        .pipe(livereload())
 });
